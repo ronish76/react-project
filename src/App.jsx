7a52c173ab8d9c2f949be7253a9ddf5cf6caf5a5
ronish <ronish.magar76@gmail.com>
2024-01-22 15:47:32 +0545

    add files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addToCount = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+    console.log(count);
+  };
+
+  const subtractFromCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{count}</h1>
+      <button onClick={subtractFromCount}>-</button>
+      <button onClick={addToCount}>+</button>
+    </div>
+  );
+}
+
+export default App
